3f28815927e90b071da15552027e1a4bba35b862
Abdurrahman-Hassan <sufyanhassan123321@gmail.com>
2024-02-13 13:57:36 +0500

    set

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,41 +24,41 @@ const index = () => {
 
   const router = useRouter();
   return (
-    <div className="bg-gray-100">
-      <div className="">
-        <h1 className="mt-[20px] ml-[20px] w-[680px] border-b-[3px] border-yellow-500  text-6xl  ">
-          Welcome to CJ Ma Blawg
-        </h1>
-      </div>
-      <div>
-        <h1 className="text-center mt-[20px] text-4xl">
-          Share Your Blog 
-        </h1>
-      </div>
-      <div className="flex justify-center items-center mt-[20px] ">
-        <button
-          onClick={() => {
-            router.push("/submit");
-          }}
-          className="pushable"
-        >
-          <span className="shadow"></span>
-          <span className="edge"></span>
-          <span className="front">Create Blog</span>
-        </button>
-      </div>
-      <div>
-        <h1 className="mt-[20px] text-center text-4xl">
-          See Our Thoughts And Stories Here
-        </h1>
-      </div>
-      <div className="mb-[20px] flex overflow-x-scroll whitespace-nowrap justify-start ml-[20px] gap-[30px]">
-        {fetchdata?.map((bloginfo, index) => {
-          return <Blogs key={index} data={bloginfo} />;
-        })}
-      </div>
-      <div className="flex justify-center">
-        <Footer/>
+    <div className="flex">
+      <div className="bg-gray-100 ">
+        <div>
+          <h1 className="mt-[20px] ml-[20px] w-[680px] border-b-[3px] border-yellow-500  text-6xl  ">
+            Welcome to CJ Ma Blawg
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-center mt-[20px] text-4xl">Share Your Blog</h1>
+        </div>
+        <div className="flex justify-center items-center mt-[20px] ">
+          <button
+            onClick={() => {
+              router.push("/submit");
+            }}
+            className="pushable"
+          >
+            <span className="shadow"></span>
+            <span className="edge"></span>
+            <span className="front">Create Blog</span>
+          </button>
+        </div>
+        <div>
+          <h1 className="mt-[20px] text-center text-4xl">
+            See Our Thoughts And Stories Here
+          </h1>
+        </div>
+        <div className="mb-[20px] flex overflow-x-scroll whitespace-nowrap justify-start ml-[20px] gap-[30px]">
+          {fetchdata?.map((bloginfo, index) => {
+            return <Blogs key={index} data={bloginfo} />;
+          })}
+        </div>
+        <div className="flex justify-center">
+          <Footer />
+        </div>
       </div>
     </div>
   );
